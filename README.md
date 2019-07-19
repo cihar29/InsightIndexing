4c52c10357c41f6a7dc9db950f4c91afc8833e00
Ubuntu <ubuntu@ip-10-0-0-13.ec2.internal>
2019-07-19 17:07:53 +0000

    updated source code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Summary
+
+I used Spark to create an inverted index from a set of text files. Each word is given an a unique id. We keep track of which files the words appear in, and output the unique id followed by a list of files.
+
+# Setup
+
+Install pyspark, get [index files](https://github.com/Samariya57/coding_challenges/tree/master/data/indexing), and run.
--- src/invertedIndex.py
+++ src/invertedIndex.py
@@ -1,11 +1,61 @@
-from pyspark.sql import SparkSession
+import re
 
-def main() :
-  text_file = sc.textFile("hdfs://...")
-  counts = text_file.flatMap(lambda line: line.split(" ")) \
-             .map(lambda word: (word, 1)) \
-             .reduceByKey(lambda a, b: a + b)
-  counts.saveAsTextFile("hdfs://...")
+from pyspark import SparkContext
+
+"""
+clean lines in a file
+lines_tup - tuple of (fname, lines)
+return - tuple of (set(cleaned word), fname)
+"""
+def clean(lines_tup) :
+    fname, lines = lines_tup
+    cleaned_words = set( re.split('\W+', lines.lower()) )
+    return (cleaned_words, fname)
+
+"""
+create list of fnames for word
+words_file - tuple of (set(word), fname)
+return - tuple of (word, list(fnames))
+"""
+def word_map(words_file) :
+    words, file = words_file
+    fname = file.split('/')[-1]
+    tup = ()
+    for word in words :
+        if word : tup += ( (word,[fname]), )
+    return tup
+
+def main(inDir, outDir) :
+    spark = SparkContext()
+
+    # get files into spark
+    files = spark.wholeTextFiles(inDir)
+
+    # clean text in files
+    # create (word, fname) map
+    # create (word, list(fnames)) map
+    # assign index to map
+    index = files.map(clean) \
+                 .map(word_map) \
+                 .flatMap(lambda x: x) \
+                 .reduceByKey(lambda x, y : x + y) \
+                 .zipWithIndex()
+
+    # output the dictionary
+    dFile = open(outDir + '/dictionary.txt','w')
+
+    dictionary = index.map(lambda x: (x[0][0],x[1])).collect()
+    for entry in dictionary :
+        dFile.write('{}\t{}\n'.format(entry[0], entry[1]))
+    dFile.close()
+
+    # create inverted index
+    index.map(lambda x : (x[1],x[0][1])) \
+         .saveAsTextFile(outDir + '/inverted_index')
+
+    spark.stop()
 
 if __name__ == "__main__":
-    main()
+    inDir = 'indexing'
+    outDir = 'output'
+    main(inDir, outDir)
